c328815a3a10a08242b400f8d1ea61bc452b0c6b
dyzio6684 <51453773+dyzio6684@users.noreply.github.com>
2023-01-14 20:06:16 +0100

    Create menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    void ExitGame()
+    {
+        Application.Quit();
+    }
+}
