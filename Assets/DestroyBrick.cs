5c5adecfc3652fdae758b5b644a944cc4edc6c66
Pawel B <pawboc12@onet.pl>
2023-01-14 23:58:49 +0100

    Added destroy bricks on collision event

--- Assets/DestroyBrick.cs
+++ Assets/DestroyBrick.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class DestroyBrick : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnCollisionEnter2D(Collision2D collision) {
+        Destroy(gameObject);
     }
 }
