bd42983a9c088c82a1ed2a97a7f2566615215ae6
dyzio6684 <51453773+dyzio6684@users.noreply.github.com>
2023-01-17 22:53:37 +0100

    Implement basic high scores system

--- Assets/Scripts/HighScores.cs
+++ Assets/Scripts/HighScores.cs
@@ -1,10 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class HighScores : MonoBehaviour
 {
+    public TextMeshProUGUI names;
+    public TextMeshProUGUI scores;
+
+    void Start()
+    {
+        var records = ScoreManager.GetScores().OrderByDescending(s => s.Score).Take(10);
+
+        var i = 1;
+        foreach (var record in records)
+        {
+            names.text += $"{i}. {record.Player}\n";
+            scores.text += $"{record.Score}\n";
+            i++;
+        }
+    }
+
     public void Return()
     {
         SceneManager.LoadScene("MainMenu");
@@ -12,6 +30,6 @@ public class HighScores : MonoBehaviour
     
     public void ClearScores()
     {
-        Debug.LogError("Not implemented");
+        ScoreManager.ClearRecords();
     }
 }
