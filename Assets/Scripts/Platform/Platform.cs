e0292df98112ecac85b5c6b2d3cfc8b168ab64af
Pawel B <pawboc12@onet.pl>
2023-01-19 19:10:49 +0100

    Added ability to move the platform with mouse movement

--- Assets/Scripts/Platform/Platform.cs
+++ Assets/Scripts/Platform/Platform.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Platform : MonoBehaviour
 {
     [SerializeField] float speed = 5f;
@@ -14,9 +15,17 @@ public class Platform : MonoBehaviour
 
     void Update() {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float mouseInput = Input.GetAxis("Mouse X");
         rb.velocity = Vector2.right * horizontalInput * speed;
 
-        if(transform.childCount > 0 && Input.GetButtonDown("Jump")) {
+        //Moves the platform to the x mouse position when mouse movement is detected
+        if (mouseInput != 0) {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            rb.transform.position = new Vector2(mousePosition.x, rb.position.y);
+        }
+
+        //Launches the ball if is grabbed by the platform
+        if (transform.childCount > 0 && Input.GetButtonDown("Jump")) {
             BallVelocity ball = GetComponentInChildren<BallVelocity>();
             ball.Launch(launchDir);
         }
