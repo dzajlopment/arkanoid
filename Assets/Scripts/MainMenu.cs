e9f606a7c5b4684eddd16fa571228a2e7bb8418a
dyzio6684 <51453773+dyzio6684@users.noreply.github.com>
2023-01-23 21:57:20 +0100

    Apply textures to UI controls

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Toggle toggle;
+
+    void Start()
+    {
+        toggle.isOn = Screen.fullScreen;
+    }
+
     public void PlayGame()
     {
         Debug.LogError("Not implemented");
@@ -23,5 +31,10 @@ public class MainMenu : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void SetFullScreen()
+    {
+        Screen.fullScreen = toggle.isOn;
     }
 }
