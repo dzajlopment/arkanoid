d249914c4414bf9101fbe115b573b156f515187f
Pawel B <pawboc12@onet.pl>
2023-02-17 19:20:04 +0100

    Add basic lifes system functions

--- Assets/Scripts/Lifes/LifesSystem.cs
+++ Assets/Scripts/Lifes/LifesSystem.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class LifesSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public static LifesSystem instance;
+    public TextMeshProUGUI lifesText;
+    int lifes = 3;
+
+    void refreshLifesText(){
+        lifesText.text = "Lifes: " + lifes.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Awake() {
+        instance = this;
     }
+
+    // Start is called before the first frame update
+    void Start() {
+        refreshLifesText();
+    }
+
+    public void addLife(){
+        lifes+=1;
+    }
+
+    public void removeLife(){
+        lifes -= 1;
+    }
+
 }
