a1c7d64d5bba6bd2e3074778be24551a90d88bcc
leviongit <levi.duncan.self@gmail.com>
2023-03-05 13:44:16 +0100

    restore changes from breaking git

--- Assets/Scripts/Scenes/Editor.cs
+++ Assets/Scripts/Scenes/Editor.cs
@@ -1,12 +1,21 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Editor : MonoBehaviour
-{
+public class Editor : MonoBehaviour {
+    private void Awake() {
+    }
+
     public void Return()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+
+    private static readonly string[] Names = {"one", "two", "three", "four", "five", "six"};
+    
+    public void CreateLevel(uint idx) {
+        if (idx > Names.Length) return;
+        Helpers.Data.LevelPersist = new Level(false, 0, Names[idx], Boosters.Default, new List<BrickData>());
+        SceneManager.LoadScene("LevelEditorScreen");
     }
 }
